6b79581308ffe4e43fbadea6fbf00e91ae5deb74
FPLCodes <sahabferdous1@gmail.com>
2025-02-10 00:59:00 +0800

    skip type checking during build

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: "dist",
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true, // Skip type checking during build
+    },
 };
 
 export default nextConfig;
